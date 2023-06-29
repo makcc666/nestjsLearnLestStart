f35afdece4f631644ca4e31c8f820a474ecca8c4
makcc666 <makcc6666@gmail.com>
2023-06-29 04:28:39 +0300

    Добавление User MongoDB Schema. Inject Mongo в User service

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,12 @@ import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
 	imports: [
 		MongooseModule.forRoot('mongodb://localhost/test'),
+		UsersModule,
 		// ConfigModule.forRoot(),
 		// AuthModule,
 		// TopPageModule,
